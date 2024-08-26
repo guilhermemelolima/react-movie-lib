652d6c5a9ea189ab8c304602c5387e9e5dc920d0
Guilherme Melo <limaguilherme.melo@gmail.com>
2024-08-26 17:17:25 -0300

    Ajustes no estilo

--- src/components/CastCard.tsx
+++ src/components/CastCard.tsx
@@ -1,17 +1,26 @@
+import { Skeleton } from "@mui/material";
+import { useState } from "react";
 import { CastType } from "../types/CastType";
-
-import './CastCard.css'
 
 const imageUrl = import.meta.env.VITE_IMG;
 
 export const CastCard = ({ cast }: { cast: CastType }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <div className="castCard">
-      <img src={imageUrl + cast.profile_path} alt={cast.name} />
-      <div className="cast-info">
-        <h4>{cast.name}</h4>
-        <p>como {cast.character}</p>
-      </div>
+      <h4>{cast.name} como {cast.character}</h4>
+      {!imageLoaded && (
+        <Skeleton variant="rectangular" width={150} height={225} />
+      )}
+      <img
+        src={imageUrl + cast.profile_path}
+        alt={cast.name}
+        style={{ display: imageLoaded ? "block" : "none" }}
+        onLoad={() => setImageLoaded(true)}
+        width={150}
+        height={225}
+      />
     </div>
   );
 };
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,19 +1,32 @@
-import { Link } from "react-router-dom"
-import { FaStar } from "react-icons/fa"
-
+import { Link } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
+import { Skeleton } from "@mui/material";
+import { useState } from "react";
 import { MovieType } from "../types/MovieType";
 
 const imageUrl = import.meta.env.VITE_IMG;
 
-export const MovieCard = ({movie, showLink} : {movie: MovieType, showLink: boolean}) => {
-    return(
-        <div className="movie-card">
-            <img src={imageUrl + movie.poster_path} alt={movie.title} />
-            <h2>{movie.title}</h2>
-            <p>
-                <FaStar id="star"/> {movie.vote_average}
-            </p>
-            {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
-        </div>
-    )
-}
+export const MovieCard = ({ movie, showLink }: { movie: MovieType, showLink: boolean }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  return (
+    <div className="movie-card">
+      {!imageLoaded && (
+        <Skeleton variant="rectangular" width={200} height={300} />
+      )}
+      <img
+        src={imageUrl + movie.poster_path}
+        alt={movie.title}
+        style={{ display: imageLoaded ? "block" : "none" }}
+        onLoad={() => setImageLoaded(true)}
+        width={200}
+        height={300}
+      />
+      <h2>{movie.title}</h2>
+      <p>
+        <FaStar id="star" /> {movie.vote_average}
+      </p>
+      {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+    </div>
+  );
+};
